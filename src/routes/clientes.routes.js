c101c2d7a06c561defa94b446eb513c85ad6d366
EdgarMrt26 <edgarmartinez09m@gmail.com>
2025-05-12 16:49:05 -0600

    .

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import {  obtenerClientes, obtenerCliente, registrarCliente, eliminarCliente, actualizarCliente } from '../controllers/clientes.controller.js';
 
 const router = Router();
-
+//
 // Ruta para obtener todos los clientes
 router.get('/clientes', obtenerClientes);
 
